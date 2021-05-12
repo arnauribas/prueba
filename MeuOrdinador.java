cb7d527bd1e9291c51a08acfe044cda8cbb2ce25
arnauribas <arnau.ribas.cmrm17@gmail.com>
2021-05-12 21:47:47 +0000

    Versió 3, amb info del sistema operatiu

--- MeuOrdinador.java
+++ MeuOrdinador.java
@@ -2,11 +2,15 @@ import java.net.*;
 public class MeuOrdinador {
 	public static void main(String[] args) {
 		try{
+			System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+			System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
 			InetAddress addr = InetAddress.getLocalHost();
 			String ipAddr = addr.getHostAddress();
 			String hostname = addr.getHostName();
 			System.out.println("hostname=" + hostname);
 			System.out.println("Adreça IP:" + ipAddr);
+			System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+			System.out.println("Versió OS: " + System.getProperty("os.version"));
 		}
 		catch(
 			UnknownHostException e) {
